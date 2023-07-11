b4e38cb44a1b082399fb780539957ced45856124
thiago-papim <thiago.fourp@gmail.com>
2023-07-11 09:48:26 -0300

    req 09

---
+++ src/Monster.ts
@@ -0,0 +1,34 @@
+import Fighter, { SimpleFighter } from './Fighter';
+
+class Monster implements SimpleFighter {
+  _lifePoints: number;
+  _strength: number;
+
+  constructor() {
+    this._lifePoints = 85;
+    this._strength = 63;
+  }
+
+  get lifePoints() { return this._lifePoints; }
+  get strength() { return this._strength; }
+
+  receiveDamage = (attackPoints: number): number => {
+    const damage = attackPoints - this._lifePoints;
+    if (damage > 0) {
+      this._lifePoints -= damage;
+    }
+    if (damage <= 0) {
+      this._lifePoints -= 1;
+    }
+    if (this._lifePoints <= 0) {
+      this._lifePoints = -1;
+    }
+    return this._lifePoints;
+  };
+
+  attack = (enemy: Fighter): void => {
+    enemy.receiveDamage(this.strength);
+  };
+}
+
+export default Monster;
